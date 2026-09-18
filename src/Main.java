a67d63cf22e817bcede91060cc3596ecd02c7698
clark7jj <clark7jj@mail.uc.edu>
2026-09-18 13:40:19 -0400

    Completed Task 01.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+double purchasePrice = 50.00; // Simulates the input value
+double salesTax = 0.00;
+
+salesTax = purchasePrice * 0.05;
+
+System.out.println("The price of your purchase is: $" + purchasePrice);
+System.out.println("The sales tax is: $" + salesTax);
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
